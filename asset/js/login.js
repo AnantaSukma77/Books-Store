4e9fbf5d4088d4a9e0e85e9ddf1f4e597d45eb1d
AnantaSukma77 <ananta.sukma77@gmail.com>
2022-08-24 18:57:29 +0700

    pembenaran css

--- asset/js/login.js
+++ asset/js/login.js
@@ -1,33 +1,36 @@
-const token = window.localStorage.token
+const token = window.localStorage.token; //window get token in local storage
 if (token) {
-    window.location.href = './index.html'
+  //jiks token bernilaai true
+  window.location.href = './index.html'; //window merujuk lokasi index
 }
-const loginBtn = document.getElementById('login-btn');
+const loginBtn = document.getElementById('login-btn'); //get tombol login
 
 loginBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
-    const usernameValue = username.value;
-    const passwordValue = password.value;
+  e.preventDefault();
+  const username = document.getElementById('username'); //get element user name
+  const password = document.getElementById('password'); //get element password
+  const usernameValue = username.value; //get nilai input username
+  const passwordValue = password.value; //get nilai input password
 
-    fetch('http://128.199.238.0:3000/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            identifier: usernameValue,
-            password: passwordValue
-        })
+  fetch('http://128.199.238.0:3000/auth/login', {
+    method: 'POST', //pilih methode POST
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      identifier: usernameValue,
+      password: passwordValue,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 403) {
+        alert('Gagal Login');
+      } else {
+        return response.json();
+      }
     })
-    .then(response => {
-        if (response.status === 403){
-            alert('Gagal Login')
-        } else {
-        return response.json() }
-    })
-    .then(data => {
-        window.localStorage.setItem('token', data.token)
-        window.location.href = './index.html'
-    })
-})
+    .then((data) => {
+      window.localStorage.setItem('token', data.token);
+      window.location.href = './index.html';
+    });
+});
